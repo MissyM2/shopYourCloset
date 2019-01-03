8e7d538b19ac6412422c844a164b600141231450
Missy Maloney <missymaloney1@gmail.com>
2019-01-02 22:59:35 -0500

    setup first files and first test

--- server.js
+++ server.js
@@ -1,4 +1,59 @@
+'use strict'
+
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
 const app = express();
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
+
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+app.use(morgan('common'));
+app.use(express.json());
+
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+    return new Promise((resolve,reject) => {
+        mongoose.connect(databaseUrl, err => {
+            if(err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            })
+            .on('error', err => {
+                mongoose.disconnect();
+                reject(err);
+            });
+        });
+    });
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if(err) {
+                    return reject(err);
+                }
+                resolve();
+            })
+        })
+    })
+}
+
+if (require.main === module) {
+    runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
+
+
+//app.listen(process.env.PORT || 8080);
