f4a3494d12ae02ab4f319e221fd17522bc9109da
Missy Maloney <missymaloney1@gmail.com>
2019-01-03 08:43:56 -0500

    removed node_modules

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict'
 // here are the environment variables
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/shopthecloset';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/shopthecloset';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/shopthecloset-db';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/shopthecloset-testdb';
 exports.PORT = process.env.PORT || 8080;
