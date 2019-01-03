f398e3865c9133ca1e7cfdc034cdd9b7f52d053d
Missy Maloney <missymaloney1@gmail.com>
2019-01-03 07:44:02 -0500

    added comment to config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 'use strict'
+// here are the environment variables
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/shopthecloset';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/shopthecloset';
