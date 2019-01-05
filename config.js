ccf915995db0f08f056cd2b6f6839c10aeb53d5f
Missy Maloney <missymaloney1@gmail.com>
2019-01-05 11:08:22 -0500

    edited URL for both databases.  They are on mLab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict'
 // here are the environment variables
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://<dbuser>:<dbpassword>@ds139632.mlab.com:39632/shopyourcloset-db1';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/shopthecloset-testdb';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:Daisl9515@ds139632.mlab.com:39632/shopyourcloset-db1';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://admin:Daisl9515@ds045021.mlab.com:45021/shopyourcloset-testdb1';
 exports.PORT = process.env.PORT || 8080;
