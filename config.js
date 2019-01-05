f31f4d0afb87ce238715a2fabfed3359a2ae8be7
Missy Maloney <missymaloney1@gmail.com>
2019-01-05 10:58:16 -0500

    changed to mlab db from localhost

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict'
 // here are the environment variables
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/shopthecloset-db';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://<dbuser>:<dbpassword>@ds139632.mlab.com:39632/shopyourcloset-db1';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/shopthecloset-testdb';
 exports.PORT = process.env.PORT || 8080;
