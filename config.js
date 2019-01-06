f33a137f2bc3a038b7e817105d80c1c4927afbad
Missy Maloney <missymaloney1@gmail.com>
2019-01-06 15:11:58 -0500

    corrected prod database in config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict'
 // here are the environment variables
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:Daisl9515@ds139632.mlab.com:39632/shopyourcloset-db1';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:Daisl9515@ds031587.mlab.com:31587/shopyourcloset-db';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://admin:Daisl9515@ds133252.mlab.com:33252/shopyourcloset-testdb';
 exports.PORT = process.env.PORT || 8080;
