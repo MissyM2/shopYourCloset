469e43b496de8e92fd8e04216b47840bd5478846
Missy Maloney <missymaloney1@gmail.com>
2019-02-19 16:40:13 -0500

    test

--- public/index.page.js
+++ public/index.page.js
@@ -236,7 +236,7 @@ function onSaveItemToClosetClick() {
     $('.section-closet').on('submit','#cl-save-btn', function(event) {
         event.preventDefault();
         console.log('made it to save item to closet');
-        /*
+        /* this is a comment
         const jwtToken = localStorage.getItem("jwtToken");
         const authUser = localStorage.getItem("userid");
         STORE.currentEditItem = {
